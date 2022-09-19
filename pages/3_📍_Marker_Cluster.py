270290c54bdca4e4390bc64c4a95662b7dc23e40
jerome-oppliger <113926043+jerome-oppliger@users.noreply.github.com>
2022-09-19 20:34:04 +0200

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -14,16 +14,13 @@ st.sidebar.image(logo)
 
 st.title("Analyse")
 
-with st.expander("See source code"):
-    with st.echo():
-
-        m = leafmap.Map(center=[46, 7], zoom=4)
+ m = leafmap.Map(center=[46, 7], zoom=4)
         
-        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-        regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
+regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
 
-        m.add_geojson(regions, layer_name='US Regions')
-        m.add_points_from_xy(
+m.add_geojson(regions, layer_name='US Regions')
+m.add_points_from_xy(
             cities,
             x="longitude",
             y="latitude",
@@ -31,6 +28,5 @@ with st.expander("See source code"):
             icon_names=['gear', 'map', 'leaf', 'globe'],
             spin=True,
             add_legend=True,
-        )
         
 m.to_streamlit(height=700)
